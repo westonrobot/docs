47204832080645c84021990c157a55951d8ae734
Ruixiang Du <ruixiang.du@gmail.com>
2025-05-03 13:40:12 +0800

    config: updated baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,12 +13,12 @@ const config: Config = {
   url: 'https://westonrobot.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs-next/',
+  baseUrl: '/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'westonrobot', // Usually your GitHub org/user name.
-  projectName: 'docs-next', // Usually your repo name.
+  projectName: 'docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
